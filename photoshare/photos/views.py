57e98fc3752b26a8e4c19f5a56efe0c4490dea89
Tristan Reboredo <tristhawnreboredo@gmail.com>
2024-12-15 15:30:56 +0800

    Second commit

--- photoshare/photos/views.py
+++ photoshare/photos/views.py
@@ -3,8 +3,15 @@ from .models import Category,Photo
 
 # Create your views here.
 def gallery(request):
+    category = request.GET.get('category')
+    if category == None:
+        photos=Photo.objects.all()
+    else:
+        photos=Photo.objects.filter(category__name=category)
+        
+
     categories=Category.objects.all()
-    photos=Photo.objects.all()
+    
 
     context = {'categories': categories, 'photos': photos}
     return render(request,'photos/gallery.html',context)
@@ -14,4 +21,26 @@ def viewPhoto(request, pk):
     return render(request,'photos/photo.html', {'photo': photo})
 
 def addPhoto(request):
-    return render(request,'photos/add.html')
+    categories = Category.objects.all()
+
+    if request.method == 'POST':
+        data = request.POST
+        image = request.FILES.get('image')
+
+        if data['category'] != 'none':
+            category = Category.objects.get(id=data['category'])
+        elif data['category_new'] != '':
+            category, created = Category.objects.get_or_create(name=data['category_new'])
+        else:
+            category = None
+
+        photo = Photo.objects.create(
+                category=category,
+                description=data['description'],
+                image=image,
+            )
+
+        return redirect('gallery')
+
+    context = {'categories': categories}
+    return render(request, 'photos/add.html', context)
